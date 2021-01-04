c3d95988bbb9c47903d6dd97a1ce10f241d2cad7
Klaudia <klaudia.nazarko+data@gmail.com>
2021-01-04 14:08:11 +0100

    fix lstm class and update readme

--- LSTM_class.py
+++ LSTM_class.py
@@ -29,12 +29,13 @@ class Sequences():
 
 
 class ModelPredict():
-    def __init__(self, model, prefix, token2ind, ind2token, max_len):
+    def __init__(self, model, prefix, token2ind, ind2token, max_len, embedding=False):
         self.model = model
         self.token2ind, self.ind2token = token2ind, ind2token
         self.max_len = max_len
         self.prefix = prefix
         self.tokens_ind = prefix.tokens_ind.copy()
+        self.embedding = embedding
 
     def __repr__(self):
         return self.prefix.content
@@ -48,13 +49,16 @@ class ModelPredict():
         return single_sequence
 
     def model_predict(self):
-        model_input = self.single_data_generation()
-        return model.predict(model_input)[0]
+        if self.embedding:
+          model_input = np.array(self.tokens_ind).reshape(1,-1)
+        else:
+          model_input = self.single_data_generation()
+        return self.model.predict(model_input)[0]
 
     @staticmethod
     def add_prob_temperature(prob, temperature=1):
         prob = prob.astype(float)
-        prob_with_temperature = np.exp(np.log(prob) / temperature)
+        prob_with_temperature = np.exp(np.where(prob == 0, 0, np.log(prob + 1e-10)) / temperature)
         prob_with_temperature /= np.sum(prob_with_temperature)
         return prob_with_temperature
 
@@ -68,16 +72,16 @@ class ModelPredict():
         prob = self.model_predict()
 
         prob_with_temperature = self.add_prob_temperature(prob, temperature)
-        next_word = np.random.multinomial(1, prob_with_temperature, 1)
+        next_word = np.random.choice(len(prob_with_temperature), p=prob_with_temperature)
 
         if as_word:
-            return self.ind2token[np.argmax(next_word)]
+            return self.ind2token[next_word]
         else:
-            return np.argmax(next_word)
+            return next_word
 
-    def generate_sequence(self, k, append=False):
+    def generate_sequence(self, k, append=False, temperature=1):
         for i in range(k):
-            next_word = self.return_next_word()
+            next_word = self.return_next_word(temperature=temperature)
             self.tokens_ind.append(next_word)
         return_tokens_ind = self.tokens_ind
         return_tokens_ind = ' '.join([self.ind2token[ind] for ind in return_tokens_ind])
@@ -87,20 +91,21 @@ class ModelPredict():
 
         return self.reverse_preprocess(return_tokens_ind)
 
-    def bulk_generate_sequence(self, k, n):
+    def bulk_generate_sequence(self, k, n, temperature=1):
         for i in range(n):
-            print(self.generate_sequence(k))
+            print(self.generate_sequence(k, temperature=temperature))
             print('\n')
 
 
 class TextDataGenerator(keras.utils.Sequence):
-    def __init__(self, sequences, next_words, sequence_length, vocab_size, batch_size=32, shuffle=True):
+    def __init__(self, sequences, next_words, sequence_length, vocab_size, batch_size=32, shuffle=True, embedding=False):
         self.batch_size = batch_size
         self.sequences = sequences
         self.next_words = next_words
         self.sequence_length = sequence_length
         self.vocab_size = vocab_size
         self.shuffle = shuffle
+        self.embedding = embedding
         self.on_epoch_end()
 
     def __len__(self):
@@ -109,7 +114,14 @@ class TextDataGenerator(keras.utils.Sequence):
     def __getitem__(self, index):
         indexes = self.indexes[index * self.batch_size: (index + 1) * self.batch_size]
         sequences_batch = [self.sequences[k] for k in indexes]
-        X, y = self.__data_generation(sequences_batch)
+        next_words_batch = [self.next_words[k] for k in indexes]
+
+        if self.embedding:
+          X = np.array(sequences_batch)
+          y = keras.utils.to_categorical(next_words_batch, num_classes=self.vocab_size)
+        else:
+          X, y = self.__data_generation(sequences_batch, next_words_batch)
+
         return X, y
 
     def on_epoch_end(self):
@@ -117,12 +129,12 @@ class TextDataGenerator(keras.utils.Sequence):
         if self.shuffle == True:
             np.random.shuffle(self.indexes)
 
-    def __data_generation(self, sequences_batch):
+    def __data_generation(self, sequences_batch, next_words_batch):
         X = np.zeros((self.batch_size, self.sequence_length, self.vocab_size), dtype=np.bool)
         y = np.zeros((self.batch_size, self.vocab_size), dtype=np.bool)
 
         for i, seq in enumerate(sequences_batch):
             for j, word in enumerate(seq):
                 X[i, j, word] = 1
-                y[i, self.next_words[i]] = 1
+                y[i, next_words_batch[i]] = 1
         return X, y
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# nlg-text-generation
+# Word-Level Text Generation (NLG)
+
+**Natural Language Generation (NLG)** or Text Generation is a subfield of Natural Language Processing (NLP). Its goal is to generate meaningful phrases and sentences in the form of human-written text. It has a wide range of use cases: writing long form content (eg reports, articles), product descriptions, social media posts, chatbots etc.
+
+The goal of this project is to generate text of fairy tale using different approaches: Markov Chain, LSTM neural network and Transformers (GPT-2). Models are going to be implemented on a word-level. The content of fairy tales scraped from the Internet will be used as a text corpus.
+
+------
+
+**Table of contents:**
+
+1. **Text Generation with Markov Chain** - simple, stochastic model which predicts the next word solely based on the previous sequence.
+2. **Text Generation with Neural Networks (LSTM)** - the model stors in the memory the previous words and based on it, it calculated the probability of the next word
+3. **Text Generation with Transformers (GPT-2)** - the transformer uses a "self-attention" mechanism to predict the next word in a sentence by focusing on words that were previously seen in the model and that are related to the predicted word
+
+------
+
+**Source of fairy tales content:**
+
+* https://www.pitt.edu/~dash/folktexts.html
+* [Kaggle Fairy Tales Dataset](https://www.kaggle.com/cuddlefish/fairy-tales)
+
