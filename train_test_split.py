628ab5621c9f70f5eb0d7fa47d6ada458e316c67
Igor Krasovskiy <i.krasovskiy@gmail.com>
2021-01-11 14:59:31 +0200

    add new py file for split data Rename py file for web scraping

--- train_test_split.py
+++ train_test_split.py
@@ -1,127 +1,56 @@
 #import functions as f
 
-import requests
+import os
 import re
-from collections import Counter
-from bs4 import BeautifulSoup, NavigableString
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 #
-main_urls = ['https://www.pitt.edu/~dash/folktexts.html',
-             'https://www.pitt.edu/~dash/folktexts2.html']
-base_url = 'https://www.pitt.edu/~dash/'
+main_path = 'data/tales/'
+files = os.listdir(main_path)
+files
 
 #
-links = []
-
-for url in main_urls:
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-
-    for a in soup.find_all('a'):
-        a_href = a.get('href')
-        links.append(a_href)
+files_with_max_count = {title[:-5]: title[-5:-4] for title in files}
+files_unique = [key + str(np.random.randint(1, int(value)+1)) + '.txt' for key, value in files_with_max_count.items()]
 
 #
-internal_links = [link.split('#')[0] for link in links if link != None and 'folktexts' not in link and 'http' not in link]
-internal_links.remove('')
-internal_links = list(set(internal_links))
+sample_size = 800
+
+files_ids = list(range(len(files_unique)))
+files_ids_sample = np.random.choice(files_ids, sample_size, replace=False)
 
 #
-#!mkdir data
-
-# Checkpoint
-save_pickle(links, 'data/links.pickle')
-save_pickle(internal_links, 'data/internal_links.pickle')
+train_ids, test_ids = train_test_split(files_ids_sample, test_size=0.2, random_state=42)
 
 #
-pages = []
-total_progress = len(internal_links)
-p = 0
+train = ''
+test = ''
+eot_token = '<| end of text |>'
 
-for link in internal_links:
-    progress = p * 100 / total_progress
+for i in train_ids:
+    #tale = f.read_txt(main_path + files_unique[i])
+    tale = read_txt(main_path + files_unique[i])
+    tale = re.sub(' +', ' ', tale.replace('\n',' '))
+    train += tale + ' ' + eot_token + ' '
 
-    sub_url = base_url + link
-    sub_page = requests.get(sub_url)
-
-    pages.append(sub_page)
-
-    if p in range(0, total_progress, 20):
-        print(progress, '%')
-    p += 1
-
-# Checkpoint
-save_pickle(pages, 'data/pages.pickle')
+for i in test_ids:
+    #tale = f.read_txt(main_path + files_unique[i])
+    tale = read_txt(main_path + files_unique[i])
+    tale = re.sub(' +', ' ', tale.replace('\n',' '))
+    test += tale + ' ' + eot_token
 
 #
-#pages = f.load_pickle('data/pages.pickle')
-pages = load_pickle('data/pages.pickle')
-#internal_links = f.load_pickle('data/internal_links.pickle')
-internal_links = load_pickle('data/internal_links.pickle')
+exclude_words = [eot_token, '']
+print('train')
+#f.text_summary(train, exclude_words)
+text_summary(train, exclude_words)
+print('\ntest')
+#f.text_summary(test, exclude_words)
+text_summary(test, exclude_words)
 
 #
-soup_list = [BeautifulSoup(page.content, 'html.parser') for page in pages]
-print(len(internal_links)==len(soup_list))
-
-h2_count = {}
-h2_content = []
-
-for i in range(len(internal_links)):
-    h2_list = soup_list[i].find_all('h2')
-    h2_content.extend([h2.text.strip().lower() for h2 in h2_list])
-    h2_count[internal_links[i]] = len(h2_list)
-
-h2_content_count = Counter(h2_content).most_common()
-
-#
-h2_black_list = ['contents', 'links to related sites', 'related links', 'links to related tales', 'notes and bibliography', 'links']
-
-#
-type1 = [k for k,v in h2_count.items() if v != 0]
-type2 = [k for k,v in h2_count.items() if v == 0]
-print('Pages with h2: %d, Pages without h2: %d' % (len(type1), len(type2)))
-
-content_dict = {internal_links[i]: soup_list[i] for i in range(len(internal_links))}
-
-#
-type1_content = {}
-
-for page in type1:
-    h2_list = content_dict[page].find_all('h2')
-    h2_text = [h2.text.strip().lower() for h2 in h2_list]
-    type1_content[page] = h2_text.count('contents') + h2_text.count('table of contents')
-
-#
-type1_with_content = [k for k,v in type1_content.items() if v != 0]
-type1_without_content = [k for k,v in type1_content.items() if v == 0]
-
-print('Pages with contents section: %d, Pages without contents section: %d' % (len(type1_with_content), len(type1_without_content)))
-
-#
-#!mkdir ./data/tales
-#
-page_skip = 0
-titles = []
-num_words = []
-exclude = ['h3']
-end = soup_list[0].new_tag('hr')
-
-for page in type1_with_content:
-    type1_page_links = content_dict[page].find_all('a', attrs={'name': True})
-
-    for a in type1_page_links[1:]:
-        start = a.find_parent('h2')
-        #if start != None and f.exclude_black_list(start.get_text(), h2_black_list):
-            #title, titles = f.format_file_name(start.get_text(), titles)
-            #text, num_words = f.format_text(start, end, exclude, num_words)
-            #f.save_txt(text, 'data/tales/' + title) if num_words[-1] >= 15 else num_words.pop()
-        if start != None and exclude_black_list(start.get_text(), h2_black_list):
-            title, titles = format_file_name(start.get_text(), titles)
-            text, num_words = format_text(start, end, exclude, num_words)
-            save_txt(text, './data/tales/' + title) if num_words[-1] >= 15 else num_words.pop()
-        else:
-            page_skip += 1
-
-#
-#f.words_summary(num_words)
-words_summary(num_words)
+#f.save_txt(train, 'data/train.txt')
+save_txt(train, 'data/train.txt')
+#f.save_txt(test, 'data/test.txt')
+save_txt(test, 'data/test.txt')
